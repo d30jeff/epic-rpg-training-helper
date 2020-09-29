de3ec492a7ae312c16e99fcd79258f455290f319
Deojeff Lai <d30jeff@gmail.com>
2020-09-29 14:32:58 +0800

    Obligatory initial commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# Copy dotenv file from example
+`mv .env.example .env`
+
+Don't forget to update your .env file for Discord token value, please refer to the example
+
+# Installing dependencies
+`yarn`
+
+
+# Running the app
+`node main.js`
---
+++ main.js
@@ -0,0 +1,51 @@
+const dotenv = require('dotenv');
+dotenv.config();
+
+const Discord = require('discord.js');
+const discord = new Discord.Client();
+
+
+const DISCORD_TOKEN = process.env.DISCORD_TOKEN;
+
+if (!DISCORD_TOKEN) {
+  throw new Error('DISCORD_TOKEN is required');
+}
+
+discord.login(DISCORD_TOKEN);
+
+discord.on('ready', () => {
+  console.log(`🤖 Logged in as ${discord.user.tag}!`);
+});
+
+discord.on('message', (msg) => {
+  try {
+    if (msg.content.includes('is training in the forest!')) {
+      const chunks = msg.content.split('\n');
+
+      const question = chunks[1];
+      const targetItem = chunks[2].match(':(.*):')[0];
+
+      const matchingQuestion = new RegExp(targetItem, 'g');
+
+      const count = question.match(matchingQuestion).length || 0;
+
+      msg.channel.send(
+        `Hi, <@${msg.author.id}>! Since you're a noob. The answer is ${count} ${targetItem}`
+      );
+    }
+
+    if (msg.content.includes('is training in the river')) {
+      const chunks = msg.content.split('\n');
+      const fish = chunks[1].match(':(.*):')[1];
+
+      msg.channel.send(
+        `Hi, <@${msg.author.id}>! Since you're a noob. It looks like a **${fish.toUpperCase()}**`
+      );
+
+    }
+  } catch (e) {
+    msg.channel.send(`My developer is a noob and so am I. Unfortunately I can't be much of use to you right now. \n
+    Error details: ${e}`)
+    console.error(e);
+  }
+});
